887a79f05fee4a025ee4f17dc59cce6bcf96b185
Globyss <vlad_zhuk98@mail.ru>
2022-10-20 10:16:06 +0300

    add adaptive menu

--- index.js
+++ index.js
@@ -34,3 +34,39 @@ function destinationSlider() {
 }
 destinationSlider()
 
+const burgerBtn = document.querySelector('.hamburger')
+const hamburgerMenu = document.querySelector('.hamburger-menu')
+const closeBtn = document.querySelector('.close-icon')
+const addShadow = document.querySelector('.background-image')
+burgerBtn.onclick = (event) =>{
+    hamburgerMenu.classList.add('active')
+    document.body.classList.add('shadow-screen')
+    document.body.style.transition = '0.5s'
+}
+
+closeBtn.onclick = (event) =>{
+    hamburgerMenu.classList.remove('active')
+    document.body.classList.remove('shadow-screen')   
+}
+
+const menuLinks = document.querySelectorAll('.menu-link[data-goto]');
+if(menuLinks.length>0){
+    menuLinks.forEach(menuLink =>{
+        menuLink.addEventListener('click', onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(event){
+        const menuLink = event.target;
+        if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + window.pageYOffset;
+            
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: 'smooth'
+            });
+            event.preventDefault();
+        }
+    }
+
+}
